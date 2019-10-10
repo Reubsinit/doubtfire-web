06a8c10a0d0d55ba3ded7606c1971735653e263c
Reuben Wilson <reubenmwilson@gmail.com>
2019-10-11 08:15:02 +1100

    QUALITY: Ensure campus is validated

--- src/app/admin/states/campuses/campus-list/campus-list.component.ts
+++ src/app/admin/states/campuses/campus-list/campus-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Inject, ViewChild } from '@angular/core';
 import { currentUser, alertService } from 'src/app/ajs-upgraded-providers';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource, MatTable } from '@angular/material/table';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { CampusService } from 'src/app/api/models/campus/campus.service';
 import { Campus } from 'src/app/api/models/campus/campus';
 
@@ -15,17 +15,29 @@ export class CampusListComponent implements OnInit {
   @ViewChild(MatTable, { static: true }) table: MatTable<any>;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  // Form controls
-  abbreviation = new FormControl('');
-  name = new FormControl('');
-  sync = new FormControl('');
+  syncModes = ['timetable', 'automatic', 'manual'];
+
+  campusForm = new FormGroup({
+    abbreviation: new FormControl('', [
+      Validators.required
+    ]),
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    mode: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        this.syncModes.map(mode => {
+          return `\b${mode}|`;
+        }).toString()
+      )
+    ])
+  });
 
   // Set up the table
   columns: string[] = ['abbreviation', 'name', 'mode', 'options'];
   campuses: Campus[] = new Array<Campus>();
   dataSource = new MatTableDataSource(this.campuses);
-
-  syncModes = ['timetable', 'automatic', 'manual'];
   selectedCampus: Campus;
 
   constructor(
@@ -34,10 +46,30 @@ export class CampusListComponent implements OnInit {
     @Inject(alertService) private alertService: any
   ) { }
 
-  clearFormControls() {
-    this.abbreviation.setValue('');
-    this.name.setValue('');
-    this.sync.setValue('');
+  private clearFormControls() {
+    this.campusForm.reset({
+      name: '',
+      abbreviation: '',
+      mode: ''
+    });
+  }
+
+  private campusChanged(campus: Campus) {
+    return campus.name !== this.campusForm.get('name').value ||
+      campus.abbreviation !== this.campusForm.get('abbreviation').value ||
+      campus.mode !== this.campusForm.get('mode').value;
+  }
+
+  private assignUpdatedValues(campus: Campus) {
+    if (campus.name !== this.campusForm.get('name').value) {
+      campus.name = this.campusForm.get('name').value;
+    }
+    if (campus.abbreviation !== this.campusForm.get('abbreviation').value) {
+      campus.abbreviation = this.campusForm.get('abbreviation').value;
+    }
+    if (campus.mode !== this.campusForm.get('mode').value) {
+      campus.mode = this.campusForm.get('mode').value;
+    }
   }
 
   ngOnInit() {
@@ -47,19 +79,25 @@ export class CampusListComponent implements OnInit {
     });
   }
 
-  applyFilter(filterValue: string) {
-    // this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
   update(campus: Campus) {
-    this.campusService.update(campus).subscribe(
-      () => {
-        this.alertService.add('success', 'Campus edited', 2000);
-      },
-      error => {
-        this.alertService.add('danger', error, 2000);
-      });
-    this.selectedCampus = null;
+    if (this.campusForm.valid) {
+      if (this.campusChanged(campus)) {
+        this.assignUpdatedValues(campus);
+        this.campusService.update(campus).subscribe(
+          () => {
+            this.alertService.add('success', 'Campus edited', 2000);
+          },
+          error => {
+            this.alertService.add('danger', `Campus edit failed: ${error}`, 2000);
+          });
+        this.selectedCampus = null;
+        this.clearFormControls();
+      } else {
+        this.alertService.add('danger', 'Campus was not changed', 2000);
+      }
+    } else {
+      this.campusForm.markAllAsTouched();
+    }
   }
 
   delete(campus: Campus) {
@@ -68,15 +106,31 @@ export class CampusListComponent implements OnInit {
   }
 
   saveNew() {
-    const newCampus = new Campus({ abbreviation: this.abbreviation.value, name: this.name.value, mode: this.sync.value });
-    this.campusService.create(newCampus).subscribe(result => {
-      this.campuses.push(result);
-      this.table.renderRows();
-      this.clearFormControls();
-    });
+    if (this.campusForm.valid) {
+      // Get the new campus valus from the form group
+      const newCampus = new Campus({
+        abbreviation: this.campusForm.get('abbreviation').value,
+        name: this.campusForm.get('name').value,
+        mode: this.campusForm.get('mode').value
+      });
+
+      this.campusService.create(newCampus).subscribe(
+        result => {
+          this.alertService.add('success', 'Campus created', 2000);
+          this.campuses.push(result);
+          this.table.renderRows();
+          this.clearFormControls();
+        },
+        error => {
+          this.alertService.add('danger', `Failed to create campus: ${error}`, 2000);
+        });
+    } else {
+      this.campusForm.markAllAsTouched();
+    }
   }
 
   cancelEdit() {
+    this.clearFormControls();
     this.selectedCampus = null;
   }
 
@@ -86,5 +140,9 @@ export class CampusListComponent implements OnInit {
 
   flagEdit(item) {
     this.selectedCampus = item;
+    this.clearFormControls();
+    this.campusForm.get('name').setValue(item.name);
+    this.campusForm.get('abbreviation').setValue(item.abbreviation);
+    this.campusForm.get('mode').setValue(item.mode);
   }
 }
